66952d1f5d10b7d9eb55fe69587cf34c4734f4f1
TB5Knight <ttburdgess@gmail.com>
2026-03-08 19:51:26 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hype Pilot</h1>
+
+      <p>
+        Hype Pilot is a task planner web application designed to help users
+        organize tasks, track deadlines, and manage daily productivity.The app allows 
+        users to create tasks, view upcoming deadlines  in a calendar layout,
+         prioritize important work, and monitor their progress. 
+      </p>
+
+    
     </div>
   );
 }
